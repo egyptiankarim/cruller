d9423da06639ebbabefd924459871c756a6b224d
egyptiankarim <karim@egyptiankarim.com>
2015-12-11 00:06:49 -0500

    Fixed GA scripts.

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -4,8 +4,8 @@
   }, i[r].l=1*new Date();
   a=s.createElement(o), m=s.getElementsByTagName(o)[0];
   a.async=1;
-  a.src=g
-  ;m.parentNode.insertBefore(a,m)
+  a.src=g;
+  m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-38521422-3', 'auto');
