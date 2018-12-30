88ef134b93f394f795907ce7b120cdbf9b8174c3
Karim Said <karim@egyptiankarim.com>
2018-12-30 11:27:29 -0500

    Cleaning up routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'errors/error_404'
   root to: 'colour#index'
+
   get 'colour/index', to: 'colour#index', as: :index
   get 'show/:colour(.:format)', to: 'colour#show', as: :show
 
+  get 'errors/error_404'
   match "/404" => "errors#error_404", via: [ :get, :post, :patch, :delete ]
 end
